0b10a5c0425fcc1795a7fe18cc8ffa031afa9c94
José Emmanuel Gómez Umbral <20302062@utfv.edu.mx>
2023-10-12 18:40:39 -0600

    Add files via upload

---
+++ ApliacionmovilPato/pwa/scriptpwa.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(registration => {
+      console.log('Service Worker registrado:', registration);
+    })
+    .catch(error => {
+      console.log('Error al registrar el Service Worker:', error);
+    });
+}
